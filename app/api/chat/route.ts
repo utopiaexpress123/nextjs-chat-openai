f357a6f460833892bee0dcb8f49b1e617f0794e8
rick-sys <ricksanchezy2x@gmail.com>
2024-03-16 23:33:46 +0100

    klkl

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,15 +1,15 @@
-import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import OpenAI from 'openai'
+import { kv } from '@vercel/kv';
+import { auth } from '@/auth';
+import { nanoid } from '@/lib/utils';
+import { ReplicateStream, StreamingTextResponse } from 'ai';
+import Replicate from 'replicate';
+import { experimental_buildLlama2Prompt } from 'ai/prompts';
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY || '',
+});
 
-export const runtime = 'edge'
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
+export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const json = await req.json()
@@ -22,44 +22,24 @@ export async function POST(req: Request) {
     })
   }
 
-  if (previewToken) {
-    openai.apiKey = previewToken
+  export async function POST(req: Request) {
+    const { messages } = await req.json();
+   
+    const response = await replicate.predictions.create({
+      // You must enable streaming.
+      stream: true,
+      // The model must support streaming. See https://replicate.com/docs/streaming
+      // This is the model ID for Llama 2 70b Chat
+      version: '2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1',
+      // Format the message list into the format expected by Llama 2
+      // @see https://github.com/vercel/ai/blob/99cf16edf0a09405d15d3867f997c96a8da869c6/packages/core/prompts/huggingface.ts#L53C1-L78C2
+      input: {
+        prompt: experimental_buildLlama2Prompt(messages),
+      },
+    });
+   
+    // Convert the response into a friendly text-stream
+    const stream = await ReplicateStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
   }
-
-  const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
-  })
-
-  return new StreamingTextResponse(stream)
-}
